1f4637248fd61fe50a7dd0f7eee87245046eaba5
IT24102313 <it24102313@my.sliit.lk>
2024-10-04 00:57:30 +0530

    IT24102313Lab7Q2A.java

---
+++ Q2/IT24102313Lab7Q2A.java
@@ -0,0 +1,22 @@
+public class IT24102313Lab7Q2A{
+	
+	public static void main(String[] args){
+		
+		
+		for(int i = 0; i < 4; i++){
+			
+			for(int j = 0; j < 5; j++){
+				
+				System.out.print("$ ");
+				
+			}
+			
+			System.out.println("");
+			
+		}
+		
+		
+		
+	}
+}
+
